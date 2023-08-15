5622344e17a21a1bc09d608c3e2a5b137c47c9a8
Dimaua <dimarugby@gmail.com>
2023-08-15 17:36:52 +0300

    script.js pulled from repo

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,29 @@
-"use strict";
+/* Задание на урок:
 
+1) Первую часть задания повторить по уроку
 
-/* 1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?' */
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-console.log(numberOfFilms);
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
 
-/* 2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false */
+P.S. Функции вызывать не обязательно */
 
+'use strict';
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -21,104 +31,52 @@ const personalMovieDB = {
     actors: {},
     genres: [],
     privat: false
+};
+
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
 }
 
-/* 3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
+rememberMyFilms();
 
-Проверить, чтобы все работало без ошибок в консоли */
-
-/*let a = prompt('Один из последних просмотренных фильмов?');
-let b = prompt('На сколько оцените его?'),
-    c = prompt('Один из последних просмотренных фильмов?'),
-    d = prompt('На сколько оцените его?');
-
-
-
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB); */
-
-/* Задание на урок:
-
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
-
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять
-
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-
-4) Потренироваться и переписать цикл еще двумя способами*/
-
-
-// Код возьмите из предыдущего домашнего задания
-
-/* for (let i = 0; i < 2; i++) {
-    let a = prompt('Один из последних просмотренных фильмов?'),
-        b = prompt('На сколько оцените его?');
-
-    if (a != '' && a.length < 50 && b != '' && a != null && b != null) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    }
-    else {
-        console.log('error')
-        i--;
-    }
-
-
-
-
-
-    let count = +personalMovieDB.count;
-    if (count < 10) {
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
         console.log("Просмотрено довольно мало фильмов");
-    } else if (count >= 10 && count < 30) {
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
         console.log("Вы классический зритель");
-    } else if (count > 30) {
+    } else if (personalMovieDB.count >= 30) {
         console.log("Вы киноман");
     } else {
         console.log("Произошла ошибка");
     }
-} */
-
-
-let i = 0;
-while (i < 2){
-    let a = prompt('Один из последних просмотренных фильмов?'),
-        b = prompt('На сколько оцените его?');
-    
-        if (a != '' && a.length < 50 && b != '' && a != null && b != null) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        }
-        else {
-            console.log('error')
-            i--;
-        }
-       
-        let count = +personalMovieDB.count;
-        if (count < 10) {
-            console.log("Просмотрено довольно мало фильмов");
-        } else if (count >= 10 && count < 30) {
-            console.log("Вы классический зритель");
-        } else if (count > 30) {
-            console.log("Вы киноман");
-        } else {
-            console.log("Произошла ошибка");
-        }
-    
-    i++;
-    
 }
-console.log(personalMovieDB);
+
+detectPersonalLevel();
+
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+
+writeYourGenres();
---
+++ JS/str.js
@@ -0,0 +1,22 @@
+let str = 'test';
+console.log(str.length);
+console.log(str[2]);
+console.log(str.toUpperCase());
+console.log(str);
+
+const fruit = "Some fruit";
+console.log(fruit.indexOf('f'));
+
+const logg = "Hello world";
+console.log(logg.slice(-5));
+
+console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 2));
+
+const num = 12.2;   
+console.log(Math.round(num));
+const test = "12.2px";
+console.log(parseInt(test));
+
+console.log(parseFloat(test));
